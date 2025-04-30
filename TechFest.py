cd8e0d1eae1bf0e8162d12d9e81e076550ee3b80
FranklinLMR <rosariosanchezfranklin@gmail.com>
2025-04-30 10:08:16 -0400

    Faster animations

--- TechFest.py
+++ TechFest.py
@@ -20,15 +20,32 @@ def main(page: ft.Page):
 #Truth Card
 
     async def truthcard(e):
+        
+        #Erases Dare side
         falses.opacity = 0
         falses.update()
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         red.opacity = 0
         red.update()
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(0.3)
         Dare.opacity = 0
         Dare.update()
+
+        #Put the Card on the center
+        truths.animate_position= 500
+        truths.update()
+        truths.top = 335
+        truths.left = 500
+        
+        truths.update()
         await asyncio.sleep(1)
+        green.opacity = 0
+        green.update()
+        await asyncio.sleep(0.5)
+        Truth.opacity = 0
+        Truth.update()
+        await asyncio.sleep(0.5)
+
 
 
 
@@ -50,22 +67,32 @@ def main(page: ft.Page):
         Or.scale = 0.75
         Advise.opacity= 0
         page.update()
-        await asyncio.sleep(1.5)
+        await asyncio.sleep(0.5)
         white.opacity= 0.4
         page.update()
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         green.opacity = 0.4 
         red.opacity = 0.4
         page.update()
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         truths.opacity = 1
         falses.opacity = 1
         
         page.update()
-        await asyncio.sleep(1.5)
+        await asyncio.sleep(2)
         ContainerTitle.on_click=None
+
+        #Fast animations setting, and clickable cards
+
+        truths.animate_opacity=200
+        falses.animate_opacity=200
+        Dare.animate_opacity=200
+        Truth.animate_opacity=200
+        green.animate_opacity=200
+        red.animate_opacity=200
         truths.on_click=truthcard
         falses.on_click=darecard
+
         page.update()
 
         
@@ -177,6 +204,7 @@ def main(page: ft.Page):
                     top= 335,
                     opacity=0,
                     animate_opacity= 900,
+                    animate_position= 950,
                     on_click=None
                     
                 )
@@ -195,6 +223,7 @@ def main(page: ft.Page):
                     top= 335,
                     opacity=0,
                     animate_opacity= 900,
+                    animate_position= 950,
                 on_click=None
             )
 
